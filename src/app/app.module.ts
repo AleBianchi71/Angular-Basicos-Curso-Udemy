4586e55017d6f27f0bc8851c46c5b39a93cec4b2
AleBianchi71 <bianchialejandro998@gmail.com>
2023-03-10 12:19:55 -0300

    se agrego Modulos y Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,18 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
-
 import { AppComponent } from "./app.component";
 
-import { HeroesModule } from './herores/herores.module';
+import { HeroesModule } from "./herores/herores.module";
 import { ContadorModule } from "./contador/contador.module";
 import { CommonModule } from "@angular/common";
-
-
+import { DbzModule } from "./dbz/dbz.module";
+import { DbzService } from './dbz/services/dbz.service';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-    ],
-    providers: [],
-    bootstrap: [AppComponent],
-    imports: [BrowserModule,
-        HeroesModule,
-        CommonModule, ContadorModule]
+  declarations: [AppComponent],
+  providers: [DbzService],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, HeroesModule, CommonModule, ContadorModule, DbzModule],
 })
 export class AppModule {}
